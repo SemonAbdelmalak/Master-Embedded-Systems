9b01cd54f7e2fdfad3a5f8712830c81d857f0977
Semon Abdelmlak <semonjoseph900@gmail.com>
2022-08-24 20:01:13 +0200

    Adding the second code for home work one

---
+++ Unit2/Lesson3_C_basics/EX2_HW1/EX2_HM1.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int tst ;
+	printf("Enter a integer: ");
+	fflush(stdout);
+	scanf("%d",&tst);
+	printf("\nYou entered: %d\n",tst);
+	fflush(stdout);
+
+	return 0;
+}
